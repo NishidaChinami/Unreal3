7daceb210cb08090ba20836e4199fac306e7dd77
NishidaChinami <134473420+NishidaChinami@users.noreply.github.com>
2023-11-11 16:14:06 +0900

    delete

---
+++ Shooting/Source/Shooting/Public/EnemyBehaviortree.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "EnemyBehaviortree.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTING_API UEnemyBehaviortree : public UBehaviorTree
+{
+	GENERATED_BODY()
+
+
+	
+};
